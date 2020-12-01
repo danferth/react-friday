5aa2e938bc368f74d43604a76941dd0c918c7617
danferth <danferth@gmail.com>
2020-12-01 15:34:38 -0800

    clean up and removed Context

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from "react";
 import Cooper from "./components/Cooper";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
-// import DarkSwitch from "./components/DarkBtn";
-import { useTheme } from "./util/ThemeContext";
 const App = () => {
-  const dark = useTheme();
   return (
     <React.Fragment>
       <div className="flex flex-col min-h-screen">
-        {/* <DarkSwitch /> */}
         <div className="flex-grow flex flex-col items-center justify-evenly md:justify-center lg:flex-row lg:px-20 min-w-full z-10 bg-white dark:bg-gray-800 transition">
           <Cooper />
           <Card />
--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./global.output.css";
 
-import { ThemeProvider } from "./util/ThemeContext";
-
 const root = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   root
 );
