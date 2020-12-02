2cc0393741f118740117d9738f830dc8bbc70e80
danferth <danferth@gmail.com>
2020-12-02 15:33:28 -0800

    mobile height bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <React.Fragment>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen max-h-screen">
         <div className="flex-grow flex flex-col items-center justify-evenly md:justify-center lg:flex-row lg:px-20 min-w-full z-10 bg-white dark:bg-gray-800 transition">
           <Cooper />
           <Card />
